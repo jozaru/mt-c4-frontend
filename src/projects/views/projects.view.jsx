abd8fb96420d39b942f1df485944e9455776be28
Jorge <jzrueda@gmail.com>
2021-11-18 20:00:08 -0500

    Queries and mutations example

--- src/projects/views/projects.view.jsx
+++ src/projects/views/projects.view.jsx
@@ -1,22 +1,103 @@
 // vedors
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import { useQuery, gql } from '@apollo/client';
+import { useMutation, useQuery, gql } from '@apollo/client';
 
-const VIEWER_QUERY = gql`
-  query {
+const REPOSITORIES_QUERY = gql`
+  query MyRepositories ($first: Int!){
     viewer { 
       name
+      repositories (first: $first){
+        nodes {
+          id
+          name
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ADD_START = gql`
+  mutation AddStart($starrableId: ID!) {
+    addStar(input: {
+      starrableId: $starrableId
+    }) {
+      starrable {
+        stargazers {
+          totalCount
+        }
+      }
+    }
+  }
+`;
+
+const REMOVE_START = gql`
+  mutation RemoveStart($starrableId: ID!) {
+    removeStar(input: {
+      starrableId: $starrableId
+    }) {
+      starrable {
+        stargazers {
+          totalCount
+        }
+      }
     }
   }
 `;
 
 const Projects = () => {
-  const { data } = useQuery(VIEWER_QUERY);
+  const [first, setFirst] = useState(1);
+  const [starrableId, setStarrableId] = useState();
+  const [starrableIdRemove, setStarrableIdRemove] = useState();
+  const { data, loading, refetch } = useQuery(REPOSITORIES_QUERY, { variables: { first } });
+  const [addStart] = useMutation(ADD_START, {
+    variables: {
+      starrableId
+    },
+    refetchQueries: [
+      REPOSITORIES_QUERY,
+      'MyRepositories'
+    ]
+  });
+  const [removeStart] = useMutation(REMOVE_START, {
+    variables: {
+      starrableId: starrableIdRemove
+    },
+    refetchQueries: [
+      REPOSITORIES_QUERY,
+      'MyRepositories'
+    ]
+  });
+
+  useEffect(() => {
+    refetch()
+  }, [first]);
+
+  useEffect(() => {
+    if(starrableId) {
+      addStart();
+    }
+  }, [starrableId]);
+
+  useEffect(() => {
+    if(starrableIdRemove) {
+      removeStart();
+    }
+  }, [starrableIdRemove]);
 
   return (
     <>
-      {data?.viewer.name}
+      {loading ? '' : data.viewer.name}
+      <ul>
+        {data?.viewer?.repositories?.nodes?.map(({ name, stargazers, id }) => (
+          <li>{name} - <span>Starts: {stargazers.totalCount}</span> <button onClick={() => setStarrableId(id)}>add start</button> <button onClick={() => setStarrableIdRemove(id)}>Remove start</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setFirst(first + 1)}>Load more</button>
       <Link to="/">{'Go back home'}</Link>
     </>
   )
