939647dfef13cd1029889f79bda836ec2d9e63f0
Jorge <jzrueda@gmail.com>
2021-12-17 19:03:28 -0500

    Add udpateUser functionality

--- src/components/menu.component.jsx
+++ src/components/menu.component.jsx
@@ -1,7 +1,6 @@
 // vendors
 import React from "react";
 import { Link as RouterLink } from 'react-router-dom';
-import jwt from 'jsonwebtoken';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from "@mui/material/Box";
@@ -9,14 +8,6 @@ import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 
 const Menu = () => {
-  const token = sessionStorage.getItem('token');
-  const user = jwt.decode(token)?.user;
-
-  const handleCloseSession = () => {
-    sessionStorage.removeItem('token');
-    window.location.href = '/';
-  };
-
   return (
     <AppBar position="static">
       <Toolbar>
--- src/users/views/users.view.jsx
+++ src/users/views/users.view.jsx
@@ -1,6 +1,6 @@
 // vendors
 import React from "react";
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
 
 const USERS = gql `
   query AllUsers {
@@ -13,15 +13,35 @@ const USERS = gql `
   }
 `;
 
+const UPDATE_USER = gql`
+  mutation ActivateUser($userId: ID) {
+    activateUser(userId: $userId) {
+      _id
+      status
+    }
+  }
+`;
+
 const Users = () => {
   const { data } = useQuery(USERS);
+  const [activateUser] = useMutation(UPDATE_USER, {
+    refetchQueries: [ { query: USERS } ],
+  });
+
+  const handleActivate = (userId) => {
+    activateUser({
+      variables: {
+        userId,
+      }
+    });
+  };
 
   return <>{!data ? <></> : data?.allUsers?.map(user => (
     <>
       <div key={user._id}>
         {user.fullName} -&nbsp;
         {user.email}&nbsp;
-        {user.status === 'PENDING' ? <button>Aceptar</button> : <></>}
+        {user.status === 'PENDING' ? <button onClick={() => handleActivate(user._id)}>Aceptar</button> : <></>}
       </div>
     </>
   ))}</>
